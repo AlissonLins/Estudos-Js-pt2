f22b9d5e192a4d4814cd236996599dbb9102f0e4
Alisson Lins <alissonlinsbispo@gmail.com>
2023-01-10 18:05:25 -0300

    executando um contador de números

--- teste.js
+++ teste.js
@@ -3,8 +3,22 @@ var desconto = 0.2;
 const PRECO = 2;
 var total = PRECO - desconto;*/
 
-function soma(a, b){
+/*function soma(a, b){
     return a + b; //8
 }
+soma(3, 5);*/
 
-soma(3, 5);
+
+function returnEvenValues(array) {
+    let evenNums = [];
+    for (let i = 0; i <array.legth; i++) {
+        if (array[i] % 2 === 0) {
+        }
+        else {
+            console.log(`$array[i] não é par!`);
+        }
+    }
+    console.log('Os números pares são', evenNums);
+}
+
+let array = [1, 2, 4, 5, 7, 8];
