810b3544fde06761a0586db752c5d3012adf32c4
Alisson Lins <alissonlinsbispo@gmail.com>
2023-01-17 10:57:03 -0300

    Contador

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,22 @@
-console.log('Hello Word!')
+const currentNumberWrapper = document.getElementById('currentNumber')
+let currentNumber = 0
+
+
+function increment() {
+    currentNumber = currentNumber + 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+    if (counter >= 10) {
+        p$.innerHTML = ++counter; 
+    }
+}
+
+function decrement() {
+    currentNumber = currentNumber - 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+    if (currentNumber < -10) {
+        document.querySelector("decrementar").disabled = true;
+    }else {
+        document.querySelector("decrementar").disabled = false;
+    }
+}
+
